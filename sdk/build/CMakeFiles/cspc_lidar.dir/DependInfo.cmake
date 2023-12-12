fc3c5e9b9aa8b5fc1f25f2399ffcefc89daac2d0
PedroAlves7 <p.edro_xp@hotmail.com>
2023-12-12 00:45:14 -0300

    cscp lidar com point cloud

---
+++ sdk/build/CMakeFiles/cspc_lidar.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/calibration.cpp" "CMakeFiles/cspc_lidar.dir/calibration.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/calibration.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/lidar_data_processing.cpp" "CMakeFiles/cspc_lidar.dir/lidar_data_processing.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/lidar_data_processing.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/lidar_information.cpp" "CMakeFiles/cspc_lidar.dir/lidar_information.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/lidar_information.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/main.cpp" "CMakeFiles/cspc_lidar.dir/main.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/main.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/mtime.cpp" "CMakeFiles/cspc_lidar.dir/mtime.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/mtime.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/node_lidar.cpp" "CMakeFiles/cspc_lidar.dir/node_lidar.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/node_lidar.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/point_cloud_optimize.cpp" "CMakeFiles/cspc_lidar.dir/point_cloud_optimize.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/point_cloud_optimize.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/serial_port.cpp" "CMakeFiles/cspc_lidar.dir/serial_port.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/serial_port.cpp.o.d"
+  "/home/pedro/dev_ws/src/cspc_lidar_sdk_ros2/sdk/timer.cpp" "CMakeFiles/cspc_lidar.dir/timer.cpp.o" "gcc" "CMakeFiles/cspc_lidar.dir/timer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sdk/build/CMakeFiles/cspc_lidar.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cspc_lidar.dir/calibration.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/calibration.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/lidar_data_processing.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/lidar_data_processing.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/lidar_information.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/lidar_information.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/main.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/main.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/mtime.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/mtime.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/node_lidar.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/node_lidar.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/point_cloud_optimize.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/point_cloud_optimize.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/serial_port.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/serial_port.cpp.o.d"
+  "CMakeFiles/cspc_lidar.dir/timer.cpp.o"
+  "CMakeFiles/cspc_lidar.dir/timer.cpp.o.d"
+  "cspc_lidar"
+  "cspc_lidar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cspc_lidar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
